85dfc18911d65cc1ecb86ca8c6c76d89d3115d51
Ryan <ryan.p.kav@gmail.com>
2021-04-05 18:22:10 +0100

    type matching in ternary operator still req

--- projects/branches/src/main.rs
+++ projects/branches/src/main.rs
@@ -1,7 +1,7 @@
 fn main() 
 {
 	let condition = true;
-	let number = if condition { 5 } else { 6 };
+	let number = if condition { 5 } else { "six" };
 
 	println!("The value of number is: {}", number);
 }
