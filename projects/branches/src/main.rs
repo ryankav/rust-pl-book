23f49ba408421ce0962351e2a5b715fa042c1486
Ryan <ryan.p.kav@gmail.com>
2021-04-05 18:22:59 +0100

    reverting to previous type

--- projects/branches/src/main.rs
+++ projects/branches/src/main.rs
@@ -1,7 +1,7 @@
 fn main() 
 {
 	let condition = true;
-	let number = if condition { 5 } else { "six" };
+	let number = if condition { 5 } else { 6 };
 
 	println!("The value of number is: {}", number);
 }
