b864539fa530f20e1499fda969e9d9188fbf2c7d
Ryan <ryan.p.kav@gmail.com>
2021-04-05 18:51:18 +0100

    python style range for loop

--- projects/loops/src/main.rs
+++ projects/loops/src/main.rs
@@ -1,10 +1,9 @@
 fn main()
 {
-	let a = [10, 20, 30, 40, 50];
-	
-	for element in a.iter()
+	for number in (1..4).rev()
 	{
-		println!("the value is {}", element); 
+		println!("{}", number); 
 	}
+	println!("Liftoff!");
 }
 
