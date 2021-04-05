e43245f0da56ca35308ede02dee605540edbc9a4
Ryan <ryan.p.kav@gmail.com>
2021-04-05 11:56:17 +0100

    function with parameter

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,12 +1,10 @@
 fn main() 
 {
-	println!("Hello, world!");
-
-	another_function();
+	another_function(5);
 }
 
-fn another_function() 
+fn another_function(x: i32) 
 {
-	println!("Another function.");
+	println!("The value of x is: {}", x);
 }
 
