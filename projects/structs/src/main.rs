28052934b4745937f214cfcba79fb85141dc6e57
Ryan <ryan.p.kav@gmail.com>
2021-07-13 11:29:25 +0100

    Rect struct implementation

--- projects/structs/src/main.rs
+++ projects/structs/src/main.rs
@@ -1,14 +1,35 @@
-fn main() {
-    let width1 = 30;
-    let height1 = 50;
-
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
-    );
+#[derive(Debug)]
+struct Rectangle {
+	height: u32,
+	width: u32
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+impl Rectangle {
+	fn area(&self) -> u32 {
+		self.width * self.height
+	}	
+
+	fn can_hold(&self, other: &Rectangle) -> bool {
+		(self.width > other.width && self.height > other.height) ||
+		(self.width > other.height && self.height > other.width) 
+	}
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
